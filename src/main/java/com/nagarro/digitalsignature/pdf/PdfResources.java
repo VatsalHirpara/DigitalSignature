ef4f6947413433adcc48b5149f5f397702cd85e4
Vatsal Hirpara <vatsal.hirpara@nagarro.com>
2020-09-03 23:58:15 +0530

    add endpoints for upload and download

--- src/main/java/com/nagarro/digitalsignature/pdf/PdfResources.java
+++ src/main/java/com/nagarro/digitalsignature/pdf/PdfResources.java
@@ -1,13 +1,27 @@
 package com.nagarro.digitalsignature.pdf;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nagarro.digitalsignature.signature.SigningService;
 
@@ -20,6 +34,9 @@ public class PdfResources {
 
     private final PdfService pdfService;
     private final SigningService signingService;
+    
+	static String fileBasePath="C:\\Users\\vatsalhirpara\\Downloads\\documentsign\\documentsign\\src\\main\\resources\\files\\";
+
 
     public PdfResources(PdfService pdfService, SigningService signingService) {
         this.pdfService = pdfService;
@@ -39,4 +56,48 @@ public class PdfResources {
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    
+    
+    
+    @PostMapping(value = "/signpdf", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity signPdfDocument(@RequestParam("file") MultipartFile file)
+			throws IllegalStateException, IOException {
+		
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		Path path = Paths.get(fileBasePath + fileName);
+		try {
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+			
+		
+		
+		
+		
+		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/download/")
+				.path(fileName)
+				.toUriString();
+		return ResponseEntity.ok(fileDownloadUri);
+	}
+    
+    
+    
+    @GetMapping("/download/{fileName:.+}")
+	public ResponseEntity<?> downloadFileFromLocal(@PathVariable String fileName) {
+		Path path = Paths.get(fileBasePath + fileName);
+		Resource resource = null;
+		try {
+			resource = new UrlResource(path.toUri());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
+	}
 }
